3b890bad0bfbf22bd6fe9c514676c5cb444a6ea3
Alexey <a.tupikov.miel@gmail.com>
2022-11-08 19:24:48 +0100

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello kakashoid");
+        System.out.println("Hello kakashoid щзф");
         System.out.println("My name is [censored]");
 
     }
