eac74bd35317b99896537e5b06fa0f25aee9d344
Aleksei Tupikov <a.tupikov.miel@gmail.com>
2022-11-10 20:55:32 +0100

    commit number 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello kakashoid!!!!");
-        System.out.println("My name is [censored]");
+        System.out.println("I don't have a name");
 
     }
 }
