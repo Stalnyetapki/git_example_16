e68bcfb505536ff1ff7dc390cd91ddea66d465d7
Aleksei Tupikov <a.tupikov.miel@gmail.com>
2022-11-10 21:04:58 +0100

    some changed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello kakashoid!!!!");
         System.out.println("I don't have a name");
+        System.out.println("norm");
 
     }
 }
