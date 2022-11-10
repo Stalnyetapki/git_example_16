c641111568c95b2d72dd0fc399adc705a59f581e
Aleksei Tupikov <a.tupikov.miel@gmail.com>
2022-11-10 21:04:18 +0100

    some changed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello kakashoid!!!!");
         System.out.println("My name is ALEKSEI ALEKSANDROVICH");
+        System.out.println("I from Danilov");
     }
 }
